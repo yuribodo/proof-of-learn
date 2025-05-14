6d75ca0fe5c829ec9af2edf0392bc838933ecde5
yuribodo <yemotalara@gmail.com>
2025-05-13 21:53:24 -0300

    feat: add routing for LoginPage and RegisterPage in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/pages/HomePage';
+import { LoginPage } from '@/pages/LoginPage';
+import { RegisterPage } from '@/pages/RegisterPage';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
