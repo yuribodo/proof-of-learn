85130a6c16809a1b783bba9fbdbc3d2f349ca65f
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-15 00:16:17 -0300

    feat: add create roadmap page

---
+++ frontend/src/pages/createRoadmap/index.tsx
@@ -0,0 +1,9 @@
+import CreateRoadmapForm from "./components/CreateRoadmapForm";
+
+export default function CreateRoadmap() {
+	return (
+		<div className="min-h-screen bg-[#121212] text-[#E0E0E0] px-8 py-5 flex flex-col">
+			<CreateRoadmapForm />
+		</div>
+	);
+}
