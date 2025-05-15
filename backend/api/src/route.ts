0dcccbb09a32ee417b53643107a5c347e38cea33
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-05-15 08:08:40 -0300

    refactor: adjust route file imports

--- backend/api/src/route.ts
+++ backend/api/src/route.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { SignUpController } from "../controler/SignUpController";
-import { SignInController } from "../controler/SignInController";
+import { SignInController } from "../controllers/SignInController";
+import { SignUpController } from "../controllers/SignUpController";
 
 const router = Router();
 
-
 router.post("/sign-up", SignUpController);
 router.post("/sign-in", SignInController);
-
 
 export { router };
