81a7e4818b3432c9022fba50dc666bdc6b090f33
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-05-15 22:42:03 -0300

    feat: create `GetAllUserRoadmapsController` to get all users roadmaps

--- backend/api/src/route.ts
+++ backend/api/src/route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { SignInController } from "../controllers/auth/SignInController";
 import { SignUpController } from "../controllers/auth/SignUpController";
+import { GetAllUserRoadmapsController } from "../controllers/roadmaps/GetAllUserRoadmapsController";
+import { AuthenticationMiddleware } from "../middlewares/AuthenticationMiddleware";
 
 const router = Router();
 
 router.post("/sign-up", SignUpController);
 router.post("/sign-in", SignInController);
+
+router.get("/roadmaps", AuthenticationMiddleware, GetAllUserRoadmapsController);
 
 export { router };
