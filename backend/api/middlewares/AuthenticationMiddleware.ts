d01a78ddfd502ed041883234ebeb933aff76eeb7
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-05-15 22:41:06 -0300

    refactor: adjust AuthenticationMiddleware to use the application ApiResponse

--- backend/api/middlewares/AuthenticationMiddleware.ts
+++ backend/api/middlewares/AuthenticationMiddleware.ts
@@ -1,33 +1,39 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { ApiResponse } from "../@types/response";
 
 export async function AuthenticationMiddleware(
 	req: Request,
 	res: Response,
-	next: NextFunction,
+	next: NextFunction
 ) {
+	const unauthorizedResponse: ApiResponse<null> = {
+		data: null,
+		error: { message: "Unauthorized", status: 401 },
+	};
+
 	if (!req.headers.authorization) {
-		res.status(401).send({ message: "Unauthorized" });
+		res.status(401).send(unauthorizedResponse);
 		return;
 	}
 
 	const token = req.headers.authorization.split(" ")[1];
 
 	if (!token) {
-		res.status(401).send({ message: "Unauthorized" });
+		res.status(401).send(unauthorizedResponse);
 		return;
 	}
 
 	try {
 		const payload = jwt.verify(
 			token,
-			process.env.JWT_SECRET as string,
+			process.env.JWT_SECRET as string
 		) as jwt.JwtPayload;
 
 		req.userId = payload.id;
 		next();
 	} catch {
-		res.status(401).send({ message: "Unauthorized" });
+		res.status(401).send(unauthorizedResponse);
 		return;
 	}
 }
