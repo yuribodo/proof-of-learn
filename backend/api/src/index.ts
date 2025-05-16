fe322f2d5dc083d574d73179ad0178934061549f
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-05-15 22:40:13 -0300

    refactor: add `/api` to all routes

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -3,7 +3,7 @@ import { router } from "./route";
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use("/api", router);
 
 app.listen(3000, () => {
 	console.log("Server is running on http://localhost:3000");
