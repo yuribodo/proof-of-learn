fd4cb336e1b674d7b90ef7aa8320146058489ec6
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-17 01:47:02 -0300

    fix: update register schema

--- frontend/src/pages/RegisterPage/schemas/registerSchema.ts
+++ frontend/src/pages/RegisterPage/schemas/registerSchema.ts
@@ -1,10 +1,17 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-	name: z.string().min(1, "Name is required"),
+	name: z.string().min(1, "Name is required").min(3, {
+		message: 'The "name" field must have at least 3 characters',
+	}),
 	email: z.string().min(1, "Email is required").email("Invalid Email Format"),
 	password: z
 		.string()
 		.min(1, "Password is required")
 		.min(6, 'The "password" field must have at least 6 characters'),
+	wallet_address: z
+		.string()
+		.min(1, 'The "Wallet Address" field is required')
+		.min(3, 'The "Wallet Address" field must have at least 3 characters')
+		.optional(),
 });
