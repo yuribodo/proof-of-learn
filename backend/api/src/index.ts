fd67c6b3cb008e3561c3498952271e4eb7f15767
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-05-17 10:08:28 -0300

    chore: add cors backend config

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,6 +1,15 @@
+import cors from "cors";
 import express from "express";
 import { router } from "./route";
+
 const app = express();
+
+app.use(
+	cors({
+		origin: process.env.FRONTEND_BASE_URL,
+		credentials: true,
+	})
+);
 
 app.use(express.json());
 app.use("/api", router);
