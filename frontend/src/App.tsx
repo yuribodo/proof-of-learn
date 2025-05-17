c25b8ac2160ffa8ec03879ed084c0b996bbfdf24
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-17 18:36:12 -0300

    feat: add context an toast to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,27 @@ import { LoginPage } from "@/pages/LoginPage";
 import { RegisterPage } from "@/pages/RegisterPage";
 import { RoadmapsPage } from "@/pages/RoadmapsPage";
 import { RoadmapDetailPage } from "@/pages/RoadmapDetailPage";
+import { AuthGuard } from "./contexts/auth/AuthGuard";
+import { AuthProvider } from "./contexts/auth/AuthProvider";
+import { Toaster } from "sonner";
 
 export default function App() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/roadmaps" element={<RoadmapsPage />} />
-				<Route path="/roadmap/:id" element={<RoadmapDetailPage />} />
-				<Route path="/create-roadmap/" element={<CreateRoadmap />} />
-			</Routes>
-		</BrowserRouter>
+		<AuthProvider>
+			<Toaster position="top-right" />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+
+					<Route element={<AuthGuard />}>
+						<Route path="/roadmaps" element={<RoadmapsPage />} />
+						<Route path="/roadmap/:id" element={<RoadmapDetailPage />} />
+						<Route path="/create-roadmap/" element={<CreateRoadmap />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</AuthProvider>
 	);
 }
