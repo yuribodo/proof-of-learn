118957fd3750341f15c373fff4d4a2db21084418
yuribodo <yemotalara@gmail.com>
2025-05-16 23:00:31 -0300

    feat: integrate React Query for data fetching with QueryClientProvider and ReactQueryDevtools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // biome-ignore lint/style/noNonNullAssertion: default setup
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</StrictMode>,
 );
