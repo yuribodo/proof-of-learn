8cd735b9dc1deca5cd107d6c5dc7aa2f022a3058
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-05-17 02:29:40 -0300

    chore: add hardcoded cors vite handling

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { router } from "./route";
+import cors from "cors"
+
 const app = express();
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use("/api", router);
+
 
 app.listen(3000, () => {
 	console.log("Server is running on http://localhost:3000");
